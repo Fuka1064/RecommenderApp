5b110550c1180fb0c2458017312e9f0cef44cd3c
fuka1064 <fuka72142@gmail.com>
2023-06-27 10:40:53 +0900

    テキストボックスを追加

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.title('ここにタイトルを表示')
-st.write('ここに説明文を表示')
+st.title('あなたへのおすすめ')
+st.write('作品を入力するとそれに基づいた他作品の紹介を行います')
+
+with st.form(key='recommend_form'):
+    # テキストボックス
+    work1 = st.text_input('好きな作品1', key='work1')
+    work2 = st.text_input('好きな作品2', key='work2')
+    work3 = st.text_input('好きな作品3', key='work3')
+
+    # ボタン
+    submit_btn = st.form_submit_button('送信')
+
+    if submit_btn:
+        works = [work1, work2, work3]
+        for i, work in enumerate(works):
+            st.text(f'あなたの好きな作品{i+1}：『{work}』')
