87365496f9fdfce8dff5ce808d035a89f0979914
fuka1064 <fuka72142@gmail.com>
2023-06-27 11:12:58 +0900

    ChatGPTを使用するためにプロンプトを作成する関数を追加

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from functions import ask_chatgpt
+from functions import ask_chatgpt, make_prompt
 
 st.title('あなたへのおすすめ')
 st.write('作品を入力するとそれに基づいた他作品の紹介を行います')
 
 with st.form(key='recommend_form'):
     # テキストボックス
+    st.write('好きな作品を入力してください')
     work1 = st.text_input('好きな作品1', key='work1')
     work2 = st.text_input('好きな作品2', key='work2')
     work3 = st.text_input('好きな作品3', key='work3')
@@ -14,10 +15,16 @@ with st.form(key='recommend_form'):
     submit_btn = st.form_submit_button('送信')
 
     if submit_btn:
+        st.write('あなたの入力内容')
         works = [work1, work2, work3]
         for i, work in enumerate(works):
             st.text(f'あなたの好きな作品{i+1}：『{work}』')
+        st.write('\n')
         
+        text = make_prompt(works)
+        print(text)
+        
+        st.write('あなたへのおすすめ')
         recommendation = ask_chatgpt(works)
         for i, work in enumerate(recommendation):
             st.text(f'あなたにおすすめの作品{i+1}：『{work}』')
--- functions.py
+++ functions.py
@@ -1,4 +1,25 @@
-def ask_chatgpt(works):
+def make_prompt(works):
+    text = f"""Here are the titles of the works I like:
+        "{works[0]}"\n
+        "{works[1]}"\n
+        "{works[2]}"\n
+        Could you point out any shared characteristics among these pieces and recommend similar works that exhibit those qualities? Even if it's difficult, I would appreciate your input.\n
+        Please answer in Japanese according to the following format:\n
+        【共通点】
+        (1) 共通点その1\n
+        (2) 共通点その2\n
+        (3) 共通点その3\n
+        【おすすめのアニメ】\n
+        1. 「タイトル」
+        おすすめの理由\n
+        2. 「タイトル」
+        おすすめの理由\n
+        3. 「タイトル」
+        おすすめの理由\n
+    """
+    return text
+
+def ask_chatgpt(text):
     # ここで作品の処理やおすすめの計算を行う
-    recommendations = [i for i in works] 
+    recommendations = [] 
     return recommendations
