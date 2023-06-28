5d8c621a52057ff58b092260543750cb96f0e74c
fuka1064 <fuka72142@gmail.com>
2023-06-28 10:30:53 +0900

    ChatGPTに質問を投げる処理以外は完成

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 import streamlit as st
-from functions import ask_chatgpt, make_prompt
+from functions import make_question, ask_chatgpt, answer_to_list
 
 st.title('あなたへのおすすめ')
 st.write('作品を入力するとそれに基づいた他作品の紹介を行います')
 
+num_fav_works = st.slider('入力したい作品の数を選んでください', 3, 10, 3)
+
 with st.form(key='recommend_form'):
     # テキストボックス
+    favorite_works = []
     st.write('好きな作品を入力してください')
-    work1 = st.text_input('好きな作品1', key='work1')
-    work2 = st.text_input('好きな作品2', key='work2')
-    work3 = st.text_input('好きな作品3', key='work3')
+    for i in range(num_fav_works):
+        favorite_works.append(st.text_input(f'好きな作品{i+1}', key=f'work{i+1}'))
 
     # ボタン
     submit_btn = st.form_submit_button('送信')
 
     if submit_btn:
         st.write('あなたの入力内容')
-        works = [work1, work2, work3]
-        for i, work in enumerate(works):
+        for i, work in enumerate(favorite_works):
             st.text(f'あなたの好きな作品{i+1}：『{work}』')
         st.write('\n')
         
-        text = make_prompt(works)
-        print(text)
+        # 入力した値からGPTに投げる質問を作る
+        question = make_question(favorite_works)
         
+        # 得た答えを扱いやすいように加工する
+        answer = ask_chatgpt(question)
+        similarities, recommend_works = answer_to_list(answer)
+        
+        st.write('あなたが好きな作品の共通点')
+        for i, similarity in enumerate(similarities):
+            st.text(f'共通点{i+1}')
+            st.text(similarity)
+            
         st.write('あなたへのおすすめ')
-        recommendation = ask_chatgpt(works)
-        for i, work in enumerate(recommendation):
-            st.text(f'あなたにおすすめの作品{i+1}：『{work}』')
+        for i, work in enumerate(recommend_works):
+            st.text(f'あなたにおすすめの作品{i+1}：{work[0]}')
+            st.text(f'おすすめの理由\n{work[1]}')
--- functions.py
+++ functions.py
@@ -1,25 +1,62 @@
-def make_prompt(works):
-    text = f"""Here are the titles of the works I like:
-        "{works[0]}"\n
-        "{works[1]}"\n
-        "{works[2]}"\n
-        Could you point out any shared characteristics among these pieces and recommend similar works that exhibit those qualities? Even if it's difficult, I would appreciate your input.\n
-        Please answer in Japanese according to the following format:\n
-        【共通点】
-        (1) 共通点その1\n
-        (2) 共通点その2\n
-        (3) 共通点その3\n
-        【おすすめのアニメ】\n
-        1. 「タイトル」
-        おすすめの理由\n
-        2. 「タイトル」
-        おすすめの理由\n
-        3. 「タイトル」
-        おすすめの理由\n
-    """
-    return text
+def make_question(works):
+    # 受け取った作品リストをもとに質問を作る
+    question = "Here are the titles of the anime I like:\n"
+    for work in works:
+        question += f"  『{work}』\n"
+    question += "\nPlease point out the characteristics common to the works listed above. Also, please recommend similar works that exhibit similar characteristics. As a reminder, please be sure to introduce actual works, even if they are difficult to guess.\n"
+    question += "Please answer in Japanese according to the following format:\n\n"
+    question += "【共通点】\n   (1) 共通点1\n   (2) 共通点2\n   (3) 共通点3\n\n"
+    question += "【おすすめのアニメ】\n"
+    for i in range(3):
+        question += f"   ({i+1}) 「タイトル」\n       おすすめの理由\n"
 
-def ask_chatgpt(text):
-    # ここで作品の処理やおすすめの計算を行う
-    recommendations = [] 
-    return recommendations
+    return question
+
+def ask_chatgpt(question):
+    # 引数として受け取った質問をChatGPTに投げてその返答を返す
+    answer = """【共通点】
+(1) シュール: 奇妙な場所や出来事が描かれたりします。
+
+(2) 異世界的: 現実とは異なる異世界的な要素を含んでいます。
+
+(3) 対比: 過酷な状況や困難な試練に直面することもあります。
+
+【おすすめのアニメ】
+(1) 『ブラック』
+おすすめの理由: 医療の現場でのドラマや人間模様が描かれています。
+
+(2) 『少女革命』
+おすすめの理由: 哲学的なストーリーが特徴です。
+
+(3) 『ハウル』
+おすすめの理由: スタジオジブリの作品である。"""
+
+    return answer
+
+def answer_to_list(answer):
+    # 空のリストを初期化
+    similarities = []
+    recommend_works = []
+
+    # テキストを行に分割する
+    lines = answer.split('\n')
+    lines = [i for i in lines if i != '']
+
+    # 共通点とおすすめのアニメのセクションの開始と終了のインデックスを見つける
+    start_index_similarities = lines.index("【共通点】") + 1
+    end_index_similarities = lines.index("【おすすめのアニメ】")
+    start_index_recommend = lines.index("【おすすめのアニメ】") + 1
+
+    # 共通点を抽出する
+    for i in range(start_index_similarities, end_index_similarities):
+        similarity = lines[i].split(') ')[1]
+        similarities.append(similarity)
+
+    # おすすめのアニメを抽出する
+    for i in range(start_index_recommend, len(lines)-1, 2):
+        recommend_work = [lines[i].strip(), lines[i+1].strip()]
+        recommend_work[0] = recommend_work[0].split(') ')[1]
+        recommend_work[1] = recommend_work[1].split('おすすめの理由: ')[1]
+        recommend_works.append(recommend_work)
+
+    return similarities, recommend_works
